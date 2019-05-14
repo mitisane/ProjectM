0a94687160ff8ecd5293bde02caa0b9ffe026154
DESKTOP-GTLA382\user <miti.san1201@ezweb.ne.jp>
2019-05-14 15:18:12 +0900

    エフェクト追加

---
+++ effectLesson/Assets/scripts/collisionEffects.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class collisionEffects : MonoBehaviour
+{
+    private ParticleSystem particle;
+
+    // Use this for initialization
+    void Start()
+    {
+        particle = this.GetComponent<ParticleSystem>();
+
+        particle.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (Time.timeSinceLevelLoad > 10.0f & Time.timeSinceLevelLoad < 50.0f)
+        {
+            if (collision.gameObject.tag == "Cube")
+            {
+                particle.Play(); //パーティクルの再生
+            }
+        }
+    }
+}
