a56143ad2bd2a1a9b755a0c5709f364f78003499
NKC_MitisaneIshiyama <miti.san1201@ezweb.ne.jp>
2019-05-31 09:34:48 +0900

    エフェクト追加

---
+++ effectLesson/Assets/Player/ParticalCon.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticalCon : MonoBehaviour {
+
+	// Use this for initialization
+
+	void Start () {
+
+        GameObject.Find("ParticalSystem");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
