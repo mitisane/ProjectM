9ae2b75c01def18f19ab61cb27a81e3e9dec8d8f
IshiyamaMitisane <41464254+mitisane@users.noreply.github.com>
2019-06-03 02:48:37 +0900

    エフェクト追加

---
+++ effectLesson/Assets/eF_scripts/circle01.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class circle01 : MonoBehaviour
+{
+
+    public float speed;
+    public float radius;
+    public float zPosition;
+    float x;
+    float y;
+    float z;
+
+    // Use this for initialization
+    void Start()
+    {
+        speed = 5.0f;
+        radius = 1.0f;
+        zPosition = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = radius * Mathf.Sin(Time.time * speed);
+        y = radius * Mathf.Cos(Time.time * speed);
+        z = zPosition;
+
+        transform.position = new Vector3(x, y+5, z);
+    }
+}
